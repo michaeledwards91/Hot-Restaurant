5f871e5a55df88c7fa71392419679862de1d09b7
michaeledwards91 <michael.edwards1991@gmail.com>
2016-12-06 10:43:52 -0600

    Routes mapped

--- server.js
+++ server.js
@@ -29,9 +29,18 @@ app.get("/reserve", function(req, res) {
 	res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.post("/api/newtable", function(req, res) {
-	
+app.get("/api/tables", function(req, res) {
+	//respond json of list of current 5 tables
+});
+
+app.get("/api/waitlist", function(req, res) {
+	//respond json list of waitlist tables
 })
+
+app.post("/api/newtable", function(req, res) {
+	var newTable = req.body;
+	console.log(newTable);
+});
 
 
 app.listen(PORT, function() {
