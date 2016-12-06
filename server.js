1f998dca58cdbb73b3c2c8ec6b1bdcca8e554684
michaeledwards91 <michael.edwards1991@gmail.com>
2016-12-06 10:38:34 -0600

    Initial backend files.

---
+++ server.js
@@ -0,0 +1,39 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+//routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+	res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+	res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.post("/api/newtable", function(req, res) {
+	
+})
+
+
+app.listen(PORT, function() {
+	console.log("Server listening on port " + PORT);
+})
